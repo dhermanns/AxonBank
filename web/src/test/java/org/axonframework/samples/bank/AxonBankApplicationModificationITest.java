2b8af43e24fe6bdda7b0a8eda4896c7c762a54dd
Dirk Hermanns <d.hermanns@lvm.de>
2017-03-23 17:47:28 +0100

    Removed unused wirings.

--- web/src/test/java/org/axonframework/samples/bank/AxonBankApplicationModificationITest.java
+++ web/src/test/java/org/axonframework/samples/bank/AxonBankApplicationModificationITest.java
@@ -34,10 +34,6 @@ public class AxonBankApplicationModificationITest {
 
     @Autowired
     private CommandBus commandBus;
-    @Autowired
-    private Repository<BankAccount> repository;
-    @Autowired
-    private TransactionManager transactionManager;
 
     @Test
     public void testMassiveModifySubAccount() {
